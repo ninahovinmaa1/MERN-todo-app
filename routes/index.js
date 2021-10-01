d559b922be15d233f36d0a6cf9cf22b983dbd229
Nina <>
2021-10-01 13:31:45 +0200

    add basic route for index. display data in json-format

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const todoLists = [ 
+    {
+        todoList1: {
+            itemId: 1,
+            title: "one",
+            content: "1st item in todo-list"
+        }
+    },
+
+    {
+        todoList2: {
+            itemId: 2,
+            title: "two",
+            content: "2nd item in todo-list"
+        }
+    }, 
+
+    {
+        todoList3: {
+            itemId: 3,
+            titel: "three",
+            content: "3rd item in todo-list"
+        }
+    }
+]
+
+/* GET all todo-lists */
 router.get('/', function(req, res, next) {
-  res.end('hello world')
+  res.json(todoLists)
 });
 
 module.exports = router;
