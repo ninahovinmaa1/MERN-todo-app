e5451ba6cb3aeecb8511caa2573c3bb1f0b69bb9
Nina <>
2021-10-01 15:14:41 +0200

    change datastructure, add route for each specific list

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 const todoLists = [ 
-    {
-        todoList1: {
+    
+        {
             itemId: 1,
             title: "one",
             content: "1st item in todo-list"
-        }
-    },
-
-    {
-        todoList2: {
+        },
+    
+        {
             itemId: 2,
             title: "two",
             content: "2nd item in todo-list"
-        }
-    }, 
+        },
 
-    {
-        todoList3: {
+        {
             itemId: 3,
             titel: "three",
             content: "3rd item in todo-list"
         }
-    }
 ]
 
 /* GET all todo-lists */
 router.get('/', function(req, res, next) {
   res.json(todoLists)
 });
+
+/*GET a specific todo-list */
+router.get('/:id', function(req, res) {
+    const id = parseInt(req.params.id);
+    const listId = todoLists.find((list) => {
+        return list.itemId === id; 
+    })
+    res.json(listId)
+
+})
 
 module.exports = router;
