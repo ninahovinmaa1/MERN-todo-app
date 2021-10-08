75b4b92515c0b45a0ff7cd31336dbfc54044ca28
Nina <>
2021-10-08 16:35:43 +0200

    add logic for post rout to be able to create to do-lists

--- routes/index.js
+++ routes/index.js
@@ -6,35 +6,61 @@ const todoLists = [
         {
             itemId: 1,
             title: "one",
-            content: "1st item in todo-list"
+            content: "1st item in todo-list",
+            dateLastEdited: 123
         },
     
         {
             itemId: 2,
             title: "two",
-            content: "2nd item in todo-list"
+            content: "2nd item in todo-list",
+            dateLastEdited: 123
         },
 
         {
             itemId: 3,
             titel: "three",
-            content: "3rd item in todo-list"
+            content: "3rd item in todo-list",
+            dateLastEdited: 123
         }
 ]
 
 /* GET all todo-lists */
-router.get('/', function(req, res, next) {
+router.get('/api', (req, res) => {
   res.json(todoLists)
 });
 
 /*GET a specific todo-list */
-router.get('/:id', function(req, res) {
-    const id = parseInt(req.params.id);
+router.get('/api/:id', function(req, res) {
+    const id = req.params.id;
     const listId = todoLists.find((list) => {
-        return list.itemId === id; 
+        return list.itemId == id; 
     })
-    res.json(listId)
+    if (!listId) {
+        res.statusCode = 404;
+        res.statusMessage = 'Not Found'
+        res.end('Not found')
+    } else {
+        res.json(listId)
+    }
 
 })
+
+/*POST, create a new todo-list*/ //expects json in the res body {"name": "soup", "description": "delicious asian fish soup"}
+router.post('/api/', (req, res) => {
+    if (!req.body) {
+       res.statusCode = 400; 
+       return;
+    }
+    res.statusCode = 200;
+    todoLists.push(req.body);
+    res.json(todoLists);
+    
+})
+
+/* router.delete('/api/:id', (req, res) => {
+    res.statusCode = 200;
+    res.json(todoLists);
+}) */
 
 module.exports = router;
