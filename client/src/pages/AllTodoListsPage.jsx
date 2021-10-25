729bd1a4b54440f3e9a8dfc6795dfa93bdf789d1
Nina <>
2021-10-25 21:11:27 +0200

    add basic functionality to read all todo-lists

--- client/src/pages/AllTodoListsPage.jsx
+++ client/src/pages/AllTodoListsPage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function AllTodoListsPage() {
+
+  const [data, setData] = useState(null);
+
+  const fetchData = () => {
+    const url = "http://localhost:3000/api/"
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setData(data))
+  };
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div>
-      all todo lists-page
+      { data && data.map((todoList, index) => {
+        return (
+          <>
+          <h3>{todoList.title}</h3>
+          <p>{todoList.content}</p>
+          </>
+        )
+      })}
     </div>
   )
 }
