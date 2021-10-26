68adf19498c8f180a6d668d83e5836245408d017
Nina <>
2021-10-26 10:00:12 +0200

    style todolist-component

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 export default function TodoList({title, body}) {
   return (
     <>
-      <h3>{title}</h3>
-      <p>{body}</p>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{title}</h5>
+          <p class="card-text">{body}</p>
+          <a href="#" class="btn btn-primary">Go to</a>
+        </div>
+      </div>
     </>
   )
 }
