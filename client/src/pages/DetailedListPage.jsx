a76447ff51189e779d459227832e907806c622f5
Nina <>
2021-10-28 10:03:28 +0200

    add delete functionality fired on button click

--- client/src/pages/DetailedListPage.jsx
+++ client/src/pages/DetailedListPage.jsx
@@ -3,10 +3,11 @@ import TodoList from '../components/TodoList';
 
 export default function DetailedListPage(props) {
   const [data, setData] = useState(null);
+  const id = props.match.params.id
+  const url = `http://localhost:3000/api/${id}`
 
+  //GET data from API for a single todoList
   const fetchData = () => {
-    const id = props.match.params.id
-    const url = `http://localhost:3000/api/${id}`
     fetch(url)
       .then(res => res.json())
       .then(data => setData(data))
@@ -16,13 +17,27 @@ export default function DetailedListPage(props) {
     fetchData()
   }, [])
 
+  //DELETE a single todoList
+  const deleteData = () => {
+    fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    }) 
+    .then(response => response.json())
+    .then(data => console.log(data)) // Manipulate the data retrieved back, if we want to do something with it
+    .catch(err => console.log(err)) // Do something with the error
+  }
 
   return (
-    <div>
+    <section>
       <h2>This is detailed list page</h2>
       {data && 
         <TodoList {...data}/>
       }
-    </div>
+      <button>Edit</button>  
+      <button onClick={deleteData()}>Delete</button>
+    </section>
   )
 }
