6b8d25d2fb538cc1ba503ef6119c2f31260ba8de
Nina <>
2021-11-02 18:02:28 +0100

    remove comments

--- server/app.js
+++ server/app.js
@@ -10,8 +10,7 @@ const TodoList = require('./models/todoList');
 
 
 //db-configuration
-//const url = 'mongodb://localhost:27017/todoListApp';
-//const uri = process.env.DB_URI;
+
 const uri = "mongodb+srv://new-user-123:12345@cluster0.aknhk.mongodb.net/todoListApp?retryWrites=true&w=majority"
 const connect = mongoose.connect(uri, {
     useNewUrlParser: true,
