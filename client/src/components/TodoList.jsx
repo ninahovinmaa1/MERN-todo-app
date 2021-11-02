b1e83c979bac28119c6a7915a4263384fdf98b47
Nina <>
2021-11-02 12:43:51 +0100

    add feature datehandling, display latest edit-timestamp

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default function TodoList({_id, title, content}) {
+export default function TodoList({_id, title, content, updatedAt}) {
   return (
     <>
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{content}</p>
+          <p>Latest updated: {updatedAt}</p>
           <Link to={`/${_id}`} className="btn btn-primary">Go to</Link>
         </div>
       </div>
--- server/models/todoList.js
+++ server/models/todoList.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const todoListSchema = new Schema({
 
   title: {
@@ -11,11 +10,7 @@ const todoListSchema = new Schema({
   content: {
     type: String,
     required: true
-  }, 
-  dateLastEdited: {
-      type: Date
-  }
-
-})
+  } 
+}, {timestamps:true})
 
 module.exports = mongoose.model('TodoList', todoListSchema);
